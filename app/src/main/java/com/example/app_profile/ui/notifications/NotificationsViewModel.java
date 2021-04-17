474fa9a62afa802b37083f5251cf2097ecc99ae1
Hyowon Kim <79815733+hyowon1116@users.noreply.github.com>
2021-04-17 18:45:55 +0900

    Update NotificationsViewModel.java

--- app/src/main/java/com/example/app_profile/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/app_profile/ui/notifications/NotificationsViewModel.java
@@ -6,14 +6,5 @@ import androidx.lifecycle.ViewModel;
 
 public class NotificationsViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
-
-    public NotificationsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is Timetable fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
+    public NotificationsViewModel() {}
 }
