4d56163a88e181d8291d88bd355573f182f90f40
Hyowon Kim <79815733+hyowon1116@users.noreply.github.com>
2021-05-31 18:09:28 +0900

    Update NotificationsViewModel.java

--- app/src/main/java/com/example/app_profile/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/app_profile/ui/notifications/NotificationsViewModel.java
@@ -17,10 +17,8 @@ public class NotificationsViewModel extends ViewModel {
     private MutableLiveData<String> mText;
     private ArrayList<User_timetable> userData = new ArrayList<>();
 
-
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-
     }
 
     public LiveData<String> getText(User_timetable user) {
