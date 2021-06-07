f889e476e9f2463a361b62c26e1510416f97622e
Hyowon Kim <79815733+hyowon1116@users.noreply.github.com>
2021-06-08 08:52:06 +0900

    Update NotificationsFragment.java

--- app/src/main/java/com/example/app_profile/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/app_profile/ui/notifications/NotificationsFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -66,6 +67,7 @@ public class NotificationsFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 db.userDao().deleteAll();
+                Toast.makeText(getActivity(),"시간표가 초기화되었습니다",Toast.LENGTH_SHORT).show();
             }
         });
 
