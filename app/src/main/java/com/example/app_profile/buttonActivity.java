f36cd12465919260706f29082f4f25b3fd8e9736
Hyowon Kim <79815733+hyowon1116@users.noreply.github.com>
2021-06-08 08:28:45 +0900

    Update buttonActivity.java

--- app/src/main/java/com/example/app_profile/buttonActivity.java
+++ app/src/main/java/com/example/app_profile/buttonActivity.java
@@ -1,25 +1,17 @@
 package com.example.app_profile;
 
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
-
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -33,24 +25,20 @@ public class buttonActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container,  Bundle savedInstanceState) {
 
-        ViewGroup rootview = (ViewGroup)inflater.inflate(R.layout.button_main,container,false);
-        BottomNavigationView navView = (BottomNavigationView)rootview.findViewById(R.id.bottom_view);
+        ViewGroup rootview = (ViewGroup) inflater.inflate(R.layout.button_main,container,false);
+        BottomNavigationView navView = (BottomNavigationView) rootview.findViewById(R.id.bottom_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
-        //FragmentActivity activitiy = null;
-        //NavController navController_b = Navigation.findNavController((AppCompatActivity) this.getActivity(), R.id.nav_host_fragment_bottom);
 
-        NavHostFragment navHostFragment = (NavHostFragment)getChildFragmentManager().findFragmentById(R.id.nav_host_fragment_bottom);
-        if (navHostFragment!=null){
+        NavHostFragment navHostFragment = (NavHostFragment) getChildFragmentManager().findFragmentById(R.id.nav_host_fragment_bottom);
+        if (navHostFragment != null) {
             NavController navController_b = navHostFragment.getNavController();
             NavigationUI.setupActionBarWithNavController((AppCompatActivity) this.getActivity(), navController_b, appBarConfiguration);
             NavigationUI.setupWithNavController(navView, navController_b);
-
-       }
-
+        }
         return rootview;
     }
 }
